18528bd83088816d87a9e7068412a1447163b293
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-07-29 16:49:13 +0100

    rectified issue with wrong import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
-import React,{BrowserRouter as Router} from 'react';
-import Navbar from './components/navbar/Navbar'
-import Homepage from './components/Homepage/Homepage'
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+// import Navbar from './components/navbar/Navbar';
+import Homepage from './components/Homepage/Homepage';
 import Login from './components/login/Login'
 import './App.css';
 
 function App() {
   return (
-    // <Router>
-    <div className="App container-fluid">
-      {/* <Navbar/> */}
-      <Homepage/>
-      <Login />
-    </div>
-    // </Router>
+    <Router>
+      <div className="App container-fluid">
+        {/* <Navbar/> */}
+        <Switch>
+          <Homepage />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
