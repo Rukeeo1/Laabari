ca2489ae7cec91e2c6363fe7fe36dafdd25e9fe5
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-08-18 16:21:59 +0100

    can now add movies

--- frontend/src/queries/queries.js
+++ frontend/src/queries/queries.js
@@ -39,19 +39,7 @@ const updateMovie = gql`
     }
   }
 `;
-// const addMovie = gql`
-//  mutation($title:String!,$year:String!,$poster:String!,$src:String!,$synopsis:String!,$similarMovies:[String!],$creator:[String!],$cast:[String!],$genre:String!,$backgroundImage:String!){
-//    addMovie(title:$title,year:$year,poster:$poster,src:$src,synopsis:$synopsis,similarMovies:$similarMovies,creator:$creator, cast:$cast,genre:$genre,backgroundImage:$backgroundImage){
-//      title
-//    }
-//  }
-// `;
-// const addMovie = gql`
-//  mutation($title:String!,$year:String!,$poster:String!,$src:String!,$synopsis:String!,$similarMovies:[String!],$creator:[String!],$cast:[String!],$genre:String!,$backgroundImage:String!){
-//   addMovie(title: "ppppp", year: "2014", poster: "https://alexis.lindaikejisblog.com/photos/shares/5d57e2da72f96.jpg", src: "https://www.youtube.com/watch?v=YxMKUsG-U6Q",synopsis:"Aisha Buhari is shaking a major table with her Instagram birthday post to immediate past National Chairman of the All Progressives Congress APC, John Odigie-Oyegun.",similarMovies:["a movie","a another movie","a very good movie","another very good movie"],creator:["rukee","and willochs"],cast:["adesuwa etomi","banki w"],genre:"drama",backgroundImage:"https://alexis.lindaikejisblog.com/photos/shares/5d57e2da72f96.jpg") {
-//     title
-//  }
-// `;
+
 const addMovie = gql`
   mutation(
     $title: String!
@@ -62,6 +50,7 @@ const addMovie = gql`
     $creator: [String!]
     $genre: String!
     $backgroundImage: String!
+    $cast: [String!]
   ) {
     addMovie(
       title: $title
@@ -76,18 +65,16 @@ const addMovie = gql`
         "another very good movie"
       ]
       creator: $creator
-      cast: ["adesuwa etomi", "banki w"]
+      cast: $cast
       genre: $genre
       backgroundImage: $backgroundImage
+     
     ) {
       title
     }
   }
 `;
-// mutation {
-//   addMovie(title: "A young woman", year: "2014", poster: "https://alexis.lindaikejisblog.com/photos/shares/5d57e2da72f96.jpg", src: "https://www.youtube.com/watch?v=YxMKUsG-U6Q",synopsis:"Aisha Buhari is shaking a major table with her Instagram birthday post to immediate past National Chairman of the All Progressives Congress APC, John Odigie-Oyegun.",similarMovies:["a movie","a another movie","a very good movie","another very good movie"],creator:["rukee","and willochs"],cast:["adesuwa etomi","banki w"],genre:"drama",backgroundImage:"https://alexis.lindaikejisblog.com/photos/shares/5d57e2da72f96.jpg") {
-//     title
-//   }
-// }
+
+
 
 export { movieQuery, deleteMovie, updateMovie, addMovie };
