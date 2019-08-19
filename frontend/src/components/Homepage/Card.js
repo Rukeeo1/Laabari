2a03b7d1bde5b841480617064df5d998b59c3b96
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-08-19 01:32:32 +0100

    now fetching movie from backend

--- frontend/src/components/Homepage/Card.js
+++ frontend/src/components/Homepage/Card.js
@@ -1,150 +1,38 @@
 import React from 'react';
 
-function Card() {
+function Card({ movies }) {
+  // console.log(movies.map(movie =>{
+  //   console.log(movie)
+  // }))
+  console.log(movies,'these are movies');
+  if (!movies) return '';
   return (
     <>
-      <div className="card card-deck" style={{ width: '210px', margin: '4px' }}>
-        <img
-          className="card-img-top"
-          src="https://img.yts.lt/assets/images/movies/london_rampage_2018/medium-cover.jpg"
-          alt="Card"
-          width="210px"
-          height="315px"
-        />
-        <div className="card-img-overlay">
-          <h4 className="card-title">John Doe</h4>
-          <p className="card-text">about the movie i want to tell </p>
-          <a href="#" className="btn btn-primary">
-            See Profile
-          </a>
-        </div>
-      </div>
-      <div className="card card-deck" style={{ width: '210px', margin: '4px' }}>
-        <img
-          className="card-img-top"
-          src="https://img.yts.lt/assets/images/movies/Dark_Angel_1990/medium-cover.jpg"
-          alt="Card"
-        />
-        <div className="card-img-overlay">
-          <h4 className="card-title">John Doe</h4>
-          <p className="card-text">Some example text.</p>
-          <a href="#" className="btn btn-primary">
-            See Profile
-          </a>
-        </div>
-      </div>
-      <div className="card card-deck" style={{ width: '210px', margin: '4px' }}>
-        <img
-          className="card-img-top"
-          src="https://www.w3schools.com/bootstrap4/img_avatar1.png"
-          alt="Card"
-        />
-        <div className="card-img-overlay">
-          <h1 className="card-title">Avengers</h1>
-          <p className="card-text">Some example text.</p>
-          <a href="#" className="btn btn-primary">
-            See Profile
-          </a>
-        </div>
-      </div>
-      <div className="card card-deck" style={{ width: '210px', margin: '4px' }}>
-        <img
-          className="card-img-top"
-          src="https://www.w3schools.com/bootstrap4/img_avatar1.png"
-          alt="Card image"
-        />
-        <div className="card-img-overlay">
-          <h4 className="card-title">John Doe</h4>
-          <p className="card-text">Some example text.</p>
-          <a href="#" className="btn btn-primary">
-            See Profile
-          </a>
-        </div>
-      </div>
-      <div className="card card-deck" style={{ width: '210px', margin: '4px' }}>
-        <img
-          className="card-img-top"
-          src="https://www.w3schools.com/bootstrap4/img_avatar1.png"
-          alt="Card"
-        />
-        <div className="card-img-overlay">
-          <h4 className="card-title">John Doe</h4>
-          <p className="card-text">Some example text.</p>
-          <a href="#" className="btn btn-primary">
-            See Profile
-          </a>
-        </div>
-      </div>
-      <div className="card card-deck" style={{ width: '210px', margin: '4px' }}>
-        <img
-          className="card-img-top"
-          src="https://img.yts.lt/assets/images/movies/Dark_Angel_1990/medium-cover.jpg"
-          alt="Card image"
-        />
-        <div className="card-img-overlay">
-          <h4 className="card-title">John Doe</h4>
-          <p className="card-text">Some example text.</p>
-          <a href="#" className="btn btn-primary">
-            See Profile
-          </a>
-        </div>
-      </div>
-      <div className="card card-deck" style={{ width: '210px', margin: '4px' }}>
-        <img
-          className="card-img-top"
-          src="https://img.yts.lt/assets/images/movies/Dark_Angel_1990/medium-cover.jpg"
-          alt="Card image"
-        />
-        <div className="card-img-overlay">
-          <h4 className="card-title">John Doe</h4>
-          <p className="card-text">Some example text.</p>
-          <a href="#" className="btn btn-primary">
-            See Profile
-          </a>
-        </div>
-      </div>
-      <div className="card card-deck" style={{ width: '210px', margin: '4px' }}>
-        <img
-          className="card-img-top"
-          src="https://img.yts.lt/assets/images/movies/Dark_Angel_1990/medium-cover.jpg"
-          alt="Card image"
-        />
-        <div className="card-img-overlay">
-          <h4 className="card-title">John Doe</h4>
-          <p className="card-text">Some example text.</p>
-          <a href="#" className="btn btn-primary">
-            See Profile
-          </a>
-        </div>
-      </div>
-      <div className="card card-deck" style={{ width: '210px', margin: '4px' }}>
-        <img
-          className="card-img-top"
-          src="https://img.yts.lt/assets/images/movies/Dark_Angel_1990/medium-cover.jpg"
-          alt="Card image"
-        />
-        <div className="card-img-overlay">
-          <h4 className="card-title">John Doe</h4>
-          <p className="card-text">Some example text.</p>
-          <a href="#" className="btn btn-primary">
-            See Profile
-          </a>
-        </div>
-      </div>
-      <div className="card card-deck" style={{ width: '210px', margin: '4px' }}>
-        <img
-          className="card-img-top"
-          src="https://img.yts.lt/assets/images/movies/Dark_Angel_1990/medium-cover.jpg"
-          alt="Card image"
-        />
-        <div className="card-img-overlay">
-          <h4 className="card-title">John Doe</h4>
-          <p className="card-text">Some example text.</p>
-          <a href="#" className="btn btn-primary">
-            See Profile
-          </a>
-        </div>
-      </div>
+      {movies.map((movie, index) => {
+        return (
+          <>
+            <div
+              className="card card-deck"
+              style={{ width: '210px', margin: '4px' }}
+            >
+              <img
+                className="card-img-top"
+                src="https://img.yts.lt/assets/images/movies/london_rampage_2018/medium-cover.jpg"
+                alt="Card"
+                width="210px"
+                height="315px"
+              />
+              <div className="card-img-overlay">
+                <h4 className="card-title">{movie.title}</h4>
+                <p className="card-text">{movie.genre}</p>
+                <a href="#" className="btn btn-primary">
+                  See Profile
+                </a>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </>
   );
 }
