43d50521e7a976d84716c1af15fb6c34bc011f12
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-08-19 03:18:06 +0100

    redirect now working

--- frontend/src/components/Homepage/Card.js
+++ frontend/src/components/Homepage/Card.js
@@ -1,37 +1,41 @@
 import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
 function Card({ movies }) {
   console.log(movies, 'these are movies');
+
   if (!movies) return '';
   return (
     <>
       {movies.map((movie, index) => {
         return (
-          <>
-            <div
-              className="card card-deck"
-              style={{ width: '210px', margin: '4px' }}
-            >
-              <img
-                className="card-img-top"
-                src="https://img.yts.lt/assets/images/movies/london_rampage_2018/medium-cover.jpg"
-                alt="Card"
-                width="210px"
-                height="315px"
-              />
-              <div className="card-img-overlay">
-                <h4 className="card-title">{movie.title}</h4>
-                <p className="card-text">{movie.genre}</p>
-                <a href="#" className="btn btn-primary">
+          <div
+            key={index}
+            className="card card-deck"
+            style={{ width: '210px', margin: '4px' }}
+          >
+            <img
+              className="card-img-top"
+              src="https://img.yts.lt/assets/images/movies/london_rampage_2018/medium-cover.jpg"
+              alt="Card"
+              width="210px"
+              height="315px"
+            />
+            <div className="card-img-overlay">
+              <h4 className="card-title">{movie.title}</h4>
+              <p className="card-text">{movie.genre}</p>
+              <span>
+                <Link to={`/videos/${movie._id}`} className="btn btn-primary">
                   See Profile
-                </a>
-              </div>
+                </Link>
+              </span>
             </div>
-          </>
+          </div>
         );
       })}
     </>
   );
 }
+
 
 export default Card;
